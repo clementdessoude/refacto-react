25c5a0e101ed93b58604a21003ff80819a6e01e3
Rudy Quinternet <rudy.quinternet@gmail.com>
2023-12-19 12:42:40 +0100

    open new page with selected in QP

--- src/components/DomainFilter/DomainFilter.component.tsx
+++ src/components/DomainFilter/DomainFilter.component.tsx
@@ -1,13 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 interface Props {
   domains: string[]
 }
 
+function filterDomains() {
+  const countriesSelect = document.getElementById('countriesSelect') as HTMLSelectElement;
+  const classificationsSelect = document.getElementById('classificationsSelect') as HTMLSelectElement;
+  const subClassificationsSelect = document.getElementById('subClassificationsSelect') as HTMLSelectElement;
+
+  const countries = Array.from(countriesSelect.selectedOptions).map((option) => option.value);
+  const classifications = Array.from(classificationsSelect.selectedOptions).map((option) => option.value);
+  const subClassifications = Array.from(subClassificationsSelect.selectedOptions).map((option) => option.value);
+
+  return window.open(`http://localhost:3000?countries=${countries.join(',')}&classifications=${classifications.join(',')}&subClassification=${subClassifications.join(',')}`, '_blank');
+}
+
 const DomainFilter: React.FC<Props> = ({ domains }) => {
     return (
     <>
-      <select title="countriesSelect" name="countries" multiple>
+      <select id="countriesSelect" title="countriesSelect" name="countries" multiple>
         {domains
         .map((v) => v.substring(0, 2))
         .filter((e, i, l) => l.indexOf(e) === i)
@@ -17,7 +29,7 @@ const DomainFilter: React.FC<Props> = ({ domains }) => {
           </option>
         ))}
       </select>
-      <select title="classificationsSelect" name="classifications" multiple>
+      <select id="classificationsSelect" title="classificationsSelect" name="classifications" multiple>
         {domains
         .map((v) => v.substring(3, 5))
         .filter((e, i, l) => l.indexOf(e) === i)
@@ -27,7 +39,7 @@ const DomainFilter: React.FC<Props> = ({ domains }) => {
           </option>
         ))}
       </select>
-      <select title="subClassificationsSelect" name="subClassifications" multiple>
+      <select id="subClassificationsSelect" title="subClassificationsSelect" name="subClassifications" multiple>
         {domains
         .map((v) => v.substring(6))
         .filter((e, i, l) => l.indexOf(e) === i)
@@ -37,6 +49,9 @@ const DomainFilter: React.FC<Props> = ({ domains }) => {
           </option>
         ))}
       </select>
+      <button id="filterButton" title="filterButton" type="button" onClick={() => filterDomains()}>
+        Filter
+      </button>
     </>
   );
 };
